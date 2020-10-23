e391979252beae91e192e42c49e3b7f54b60e900
Carl Turner <carl@Carls-MacBook-Pro.local>
2020-10-23 17:12:20 +0100

    changed api call type to no-cors

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ app.post("/api/stopController", (req, res) => {
 app.post("/api/startController", (req, res) => {
   res.status(200).send("Started streamdeck");
   shell.exec("gnome-terminal -e 'streamdeck'");
+  shell.exec("killall google-chrome");
+  shell.exec("gnome-terminal -e 'google-chrome --kiosk http://localhost:3000");
   console.log("Started.");
 });
 
