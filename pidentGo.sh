cb39858e6898f78ac77f4ee159197d6aa971e980
Carl Turner <carl@Carls-MacBook-Pro.local>
2020-10-30 12:59:21 +0000

    updated launch sequence

--- pidentGo.sh
+++ pidentGo.sh
@@ -4,8 +4,10 @@ echo Changing directories...
 
 cd pident-spa
 
-gnome-terminal -e "node server.js"
+gnome-terminal -- "node server.js"
 
-gnome-terminal -e "npm start"
+gnome-terminal -- "npm start"
 
-gnome-terminal -e "google-chrome --kiosk http://localhost:3000"
+sleep 10
+
+gnome-terminal -- "google-chrome --kiosk http://localhost:3000"
